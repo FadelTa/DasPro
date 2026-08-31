1540a54c8718521816c74cbac137c60abfa0d8d3
abe <55230345+FadelTa@users.noreply.github.com>
2026-08-31 12:12:37 +0700

    Add files via upload

---
+++ Project/MyFirstJava/MyFirstJava11.java
@@ -0,0 +1,9 @@
+public class MyFirstJava11 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello I'm Fadel Abe Johansyah");
+        System.out.print("\nBelajar java itu mudah!");
+        //System.out.print("\nSemakin sering mecoba, semakin jago.");
+
+    }
+}
